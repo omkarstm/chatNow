8d68a871d490513391e5364ecde7dad4b00f4e51
omkaradstia <omkar@adstia.com>
2024-12-19 11:25:42 +0530

    isuee fix

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useChatStore } from "../store/useChatStore";
 import { useAuthStore } from "../store/useAuthStore";
 import { Users } from "lucide-react";
-import SidebarSkeleton from "./skeleton/SidebarSkeleton.jsx";
+import SidebarSkeleton from "./skeleton/SideBarSkeleton";
 
 const Sidebar = () => {
   const { getUsers, users, selectedUser, setSelectedUser, isUsersLoading } = useChatStore();
