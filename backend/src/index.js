c16614cb4f799cc497d0732137cc1f07d0415b66
omkaradstia <omkar@adstia.com>
2024-12-19 11:31:03 +0530

    fix route

--- backend/src/index.js
+++ backend/src/index.js
@@ -49,10 +49,10 @@ app.use('/uploads', express.static('uploads'));
 
 
 if(process.env.NODE_ENV=== "production"){
-  app.use(express.static(path.join(__dirname,"../../frontend/dist")))
+  app.use(express.static(path.join(__dirname,"../frontend/dist")))
 
   app.get("*",(req,res)=>{
-    res.sendFile(path.join(__dirname,"../../frontend","dist","index.html"))
+    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
   })
 }
 
